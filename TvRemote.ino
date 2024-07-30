d84b382865909bbd52193a7da2e1a2efc3cce771
Brydon Leonard <brydon.leonard@gmail.com>
2024-07-30 23:09:13 +0200

    It works!

--- TvRemote.ino
+++ TvRemote.ino
@@ -5,87 +5,262 @@
 #include <IRtext.h>
 #include <IRutils.h>
 
-static const int MACRO_BUTTON_COUNT = 8;
-static const int COMMANDS_PER_MACRO = 16;
+#include <Preferences.h>
 
-static const uint16_t IR_RECEIVER_PIN = 25;
+// <Configuration>
+static const int MACRO_BUTTON_COUNT = 8; // Number of buttons on the box
+static const int COMMANDS_PER_MACRO = 16; // The max commands for each macro
+
+static const uint16_t IR_RECEIVER_PIN = 25; 
 static const uint16_t IR_EMITTER_PIN = 33;
 static const int STATUS_LED_PIN_R = 32;
 static const int STATUS_LED_PIN_G = 14;
 static const int STATUS_LED_PIN_B = 13;
+// The number of pins must match MACRO_BUTTON_COUNT
 static const int MACRO_BUTTON_PINS[MACRO_BUTTON_COUNT] = {
   22, 21, 19, 18, 5, 4, 2, 15
 };
 
-
-static const uint16_t IR_RECEIVER_BUFFER_SIZE = 1024;
-static const uint8_t IR_RECEIVER_TIMEOUT = 50; // The minimum pause between two commands
+// Knobs for the IR receiver
+static const uint16_t IR_RECEIVER_BUFFER_SIZE = 1024; // Buffer for incoming commands
+static const uint8_t IR_RECEIVER_TIMEOUT = 15;  // The minimum pause between two commands
 static const uint8_t IR_MIN_UNKNOWN_SIZE = 12;
-static const uint8_t IR_RECEIVER_TOLERANCE = 50;  // kTolerance is normally 25%
+static const uint8_t IR_RECEIVER_TOLERANCE = 25;  // The percentage tolerance for bad signals. 25% is the default.
+// After we record a command, we don't accept new commands for a while. Doing so avoids recording a command multiple
+// times when the person recording a macro holds the button for too long.
+static const int IR_RECEIVER_INTER_COMMAND_PAUSE = 500; 
+static const int IR_SEND_REPEATS = 1; // The number of repeats sent with IR commands
 
-
+// I know states. I have the best states.
 static const int STATE_READY_TO_RECORD = 0;
 static const int STATE_RECORDING = 1;
 static const int STATE_READY_TO_PLAYBACK = 2;
 
-static const int RECORD_MODE_SWITCH_INDEX = 0;
 static const int RECORD_MODE_SWITCH_PIN = 23;
 
+// https://en.wikipedia.org/wiki/Switch#Contact_bounce
 static const int DEBOUNCE_DELAY_MS = 50;
+
+// Only check the switch this often during recording
+static const int SWITCH_SCAN_INTERVAL = 1000;
+
+// The length of the breaks between commands during playback
+static const int COMMAND_PLAYBACK_INTERVAL = 750;
+
+// </Configuration>
+
+// <Globals>
+Preferences preferences; // For saving macros
 
 IRsend irsend(IR_EMITTER_PIN);
 IRrecv irrecv(IR_RECEIVER_PIN, IR_RECEIVER_BUFFER_SIZE, IR_RECEIVER_TIMEOUT, true);
-decode_results results;  // Somewhere to store the results
+decode_results results;  // Somewhere to store the commands received by the IR receiver.
+// Used alonside IR_RECEIVER_INTER_COMMAND_PAUSE to record when commands should be accepted again.
+uint32_t receiver_paused_until = 0; 
 
-// Yucky global state, but this is all in one file anyway!
-// Current state of the controller
 int state = -1;
 // Tracks the next time each button's state can change to prevent bouncing.
 uint32_t debouncingUntil[MACRO_BUTTON_COUNT];
 uint32_t recordSwitchDebouncingUntil;
 bool buttonPressed[MACRO_BUTTON_COUNT];
 
-// Used to avoid checking all buttons on every cycle while recording
-uint32_t nextButtonScan;
+// Used to avoid checking the recording switch on every loop
+uint32_t nextSwitchScan;
 
 // The button that's currently being recorded.
 int recordingButtonId = -1;
 
 struct SavedCommand {
-  uint16_t size;
-  uint64_t value;
-  decode_type_t protocol;
+  uint8_t size; // 1b
+  uint64_t value; // 8b
+  decode_type_t protocol; // 1b
 };
+
 // Stores commands that are currently being recorded
 SavedCommand commands[COMMANDS_PER_MACRO];
-// A pointer to the position after the final recorded command. Once this is 16, the command array is full.
+// Used while recording - points to the index _after_ the final command in the currently recording macro
+// (i.e. this stores the length of the current macro).
 short commandPtr = 0;
 // A struct to store saved commands. These could be newly recorded or retrieved from flash storage.
 struct {
   SavedCommand savedMacros[MACRO_BUTTON_COUNT][COMMANDS_PER_MACRO];
-  short macroLength[MACRO_BUTTON_COUNT]; // 0 for unsaved commands
+  short macroLength[MACRO_BUTTON_COUNT];  // 0 for unsaved commands
 } saveData;
+
+// </Globals>
+
+/*
+ * Loads macros from flash memory.
+ * 
+ * See stashNewMacro for more information on how macros are saved.
+ */
+void loadMacros() {
+  for (int i = 0; i < MACRO_BUTTON_COUNT; i++) {
+    String keyPrefix = String(i, HEX); 
+    String lengthKey = keyPrefix + "l";
+
+    char lengthKeyCharArr[lengthKey.length() + 1];
+    lengthKey.toCharArray(lengthKeyCharArr, lengthKey.length() + 1);
+
+    uint8_t macroLength = preferences.getUChar(lengthKeyCharArr, 0);
+
+    saveData.macroLength[i] = macroLength;
+
+#ifdef DEBUG 
+    if (macroLength > 0) {
+      Serial.print("Found a saved macro for button ");
+      Serial.println(i);
+    } else {
+      Serial.print("Found no saved macro in ");
+      Serial.println(lengthKey);
+    }
+#endif
+
+    for (int j = 0; j < macroLength; j++) {
+      String commandKey = keyPrefix + String(j, HEX);
+
+      char keyCharArr[commandKey.length() + 1];
+      commandKey.toCharArray(keyCharArr, commandKey.length() + 1);
+
+      uint64_t encodedCommand = preferences.getULong64(keyCharArr, 0);
+
+      SavedCommand decodedCommand;
+      decodeCommand(encodedCommand, &decodedCommand);
+
+      saveData.savedMacros[i][j].value = decodedCommand.value;
+      saveData.savedMacros[i][j].protocol = decodedCommand.protocol;
+      saveData.savedMacros[i][j].size = decodedCommand.size;
+    }
+  }
+}
+
+/*
+ * Saves the button's macro to flash memory
+ * 
+ * This uses the Preferences library (https://tinyurl.com/esp32-preferences) to save the macro
+ * so it can be retrieved after a power cycle. The preferences library stores key-value pairs,
+ * so this method define a set of standard keys per macro. The keys are kept to two characters
+ * to minimize their storage overhead:
+ * - "{buttonIndex}l" - Stores the length of the macro (in commands)
+ * - "{buttonIndex}{commandIndex}" - Stores the encoded command at index {commandIndex} for the 
+ *   macro on button {buttonIndex}.
+ */
+void stashNewMacro(int macroButtonIndex) {
+  String keyPrefix = String(macroButtonIndex, HEX); 
+  String lengthKey = keyPrefix + "l";
+
+  char lengthKeyCharArr[lengthKey.length() + 1];
+  lengthKey.toCharArray(lengthKeyCharArr, lengthKey.length() + 1);
+
+  preferences.putUChar(lengthKeyCharArr, saveData.macroLength[macroButtonIndex]);
+
+#ifdef DEBUG
+  Serial.print("Saving macro length in ");
+  Serial.println(lengthKeyCharArr);
+#endif
+
+  for (int i = 0; i < saveData.macroLength[macroButtonIndex]; i++) {
+    String commandKey = keyPrefix + String(i, HEX);
+
+    char keyCharArr[commandKey.length() + 1];
+    commandKey.toCharArray(keyCharArr, commandKey.length() + 1);
+
+    uint64_t encodedCommand = encodeCommand(saveData.savedMacros[macroButtonIndex][i]);
+    preferences.putULong64(keyCharArr, encodedCommand);
+
+#ifdef DEBUG
+    Serial.print("Saving macro command in ");
+    Serial.println(keyCharArr);
+#endif
+  }
+}
+
+/*
+ * Nice pretty printing of commands.
+ */
+void print(SavedCommand command) {
+#ifdef DEBUG
+  Serial.print("Size: ");
+  Serial.print(command.size);
+  Serial.print(", Protocol: ");
+  Serial.print(command.protocol);
+  Serial.print(", Value: ");
+  Serial.print(command.value);
+  Serial.print(", Hex: ");
+  Serial.println(String(command.value, HEX));
+#endif
+}
+
+/*
+ * Encodes a command as a uint64.
+ * 
+ * The most significant 16 bits are used to store the command's size and protocol (8 bits each).
+ * All remaining bits then store the command value (the actual command that's transmitted to the
+ * device). This approach assumes that no command will actually be more than 32 bits; my testing
+ * has shown that to be the case, but if the code encounters some protocol that uses extra long 
+ * commands, this'll fail horribly. I decided that it's worth the tradeoff since doing it this
+ * way gives us extra space in the ESP32's flash memory to store commands.
+ */
+uint64_t encodeCommand(SavedCommand command) {
+  print(command);
+  uint64_t size = command.size;
+  size = size << 56;
+  uint64_t protocol = command.protocol;
+  protocol = protocol << 48;
+  uint64_t value = command.value;
+
+  uint64_t encoded = value | protocol | size;
+
+  return encoded;
+}
+
+/*
+ * Decodes 
+ * 
+ * This is just the opposite of encodeCommand. See that function's description for more info.
+ */
+void decodeCommand(uint64_t encodedCommand, SavedCommand* command) {
+  uint64_t mask = 0b1111111111111111;
+  mask = mask << 48;
+  mask = ~mask;
+
+  command->value = encodedCommand & mask;
+  command->size = encodedCommand >> 56;
+  command->protocol = static_cast<decode_type_t>((encodedCommand >> 48) & 0b0000000011111111);
+}
 
 void setup() {
   Serial.begin(115200);
+
+#ifdef DEBUG
+  while (!Serial)
+    delay(50);
   Serial.println("Started!\n");
+#endif
+
+  preferences.begin("tv-remote", false);
+  loadMacros();
+
+  pinMode(IR_EMITTER_PIN, OUTPUT);
   pinMode(IR_RECEIVER_PIN, INPUT);
 
+  // Tweak a couple knobs on the IR receiver and start it
   irrecv.setUnknownThreshold(IR_MIN_UNKNOWN_SIZE);
-  irrecv.setTolerance(IR_RECEIVER_TOLERANCE);  // Override the default tolerance.
-  irrecv.enableIRIn();  // Start the IR receiver
+  irrecv.setTolerance(IR_RECEIVER_TOLERANCE);
+  irrecv.enableIRIn();
 
+  // Start the IR emitter
   irsend.begin();
-  
+
+  // These are the status LED's three pins
   pinMode(STATUS_LED_PIN_R, OUTPUT);
   pinMode(STATUS_LED_PIN_G, OUTPUT);
   pinMode(STATUS_LED_PIN_B, OUTPUT);
 
-  pinMode(IR_EMITTER_PIN, OUTPUT);
-
   // Enable the mode switch
-  pinMode(RECORD_MODE_SWITCH_PIN, INPUT);
+  pinMode(RECORD_MODE_SWITCH_PIN, INPUT_PULLDOWN);
 
+  // This makes sure we start in the right state
   if (digitalRead(RECORD_MODE_SWITCH_PIN)) {
     state = STATE_READY_TO_RECORD;
     setStatus(true, true, false);
@@ -101,7 +276,7 @@ void setup() {
   }
 }
 
-// Here be debouncing
+// <BUTTONS AND DEBOUNCING>
 bool debounced(int buttonId) {
   return millis() > debouncingUntil[buttonId];
 }
@@ -118,47 +293,15 @@ void debounceRecordSwitch() {
   recordSwitchDebouncingUntil = millis() + DEBOUNCE_DELAY_MS;
 }
 
-// Status LED controls
-void setStatus(bool r, bool g, bool b) {
-  digitalWrite(STATUS_LED_PIN_R, r);
-  digitalWrite(STATUS_LED_PIN_G, g);
-  digitalWrite(STATUS_LED_PIN_B, b);
-}
-
-void resetStatus() {
-  digitalWrite(STATUS_LED_PIN_R, false);
-  digitalWrite(STATUS_LED_PIN_G, false);
-  digitalWrite(STATUS_LED_PIN_B, false);
-}
-
-// Logging
-int logStateNext = 0;
-double maxInput = -1;
-void logState() {
-  double input = analogRead(IR_RECEIVER_PIN);
-  if (input > maxInput) {
-    maxInput = input;
-  }
-  
-  if (millis() > logStateNext) {
-    Serial.print("State is ");
-    Serial.println(state);
-    Serial.print("Max input was ");
-    Serial.println(maxInput);
-    logStateNext = millis() + 1000;
-    maxInput = -1;
-  }
-}
-
 int getPressedButtonIndex() {
-  // We really can't afford to spend time scanning unnecessarily because it messes with the IR 
+  // We really can't afford to spend time scanning unnecessarily because it messes with the IR
   // receiver. As such, when recording, this only checks the button for which we're actively
-  // recording. Other times, we scan all of them.
+  // recording. Otherwise we scan all of them.
   if (recordingButtonId == -1) {
     int pressed = -1;
     for (int i = 0; i < MACRO_BUTTON_COUNT; i++) {
-      // We if multiple buttons are somehow pressed on the same cycle, 
-      // all after the first are ignored until the next cycle.
+      // If multiple buttons are somehow pressed on the same cycle, we ignore all except the first one
+      // we encounter here.
       if (pressed == -1 && debounced(i) && !buttonPressed[i] && digitalRead(MACRO_BUTTON_PINS[i])) {
         debounce(i);
         buttonPressed[i] = true;
@@ -182,8 +325,21 @@ int getPressedButtonIndex() {
 
   return -1;
 }
+// </BUTTONS AND DEBOUNCING>
 
+// <STATUS LED CONTROLS>
+void setStatus(bool r, bool g, bool b) {
+  analogWrite(STATUS_LED_PIN_R, 10 * r);
+  analogWrite(STATUS_LED_PIN_G, 10 * g);
+  analogWrite(STATUS_LED_PIN_B, 10 * b);
+}
 
+void resetStatus() {
+  analogWrite(STATUS_LED_PIN_R, false);
+  analogWrite(STATUS_LED_PIN_G, false);
+  analogWrite(STATUS_LED_PIN_B, false);
+}
+// </STATUS LED CONTROLS>
 
 void loop() {
   int pressedButtonIndex;
@@ -191,17 +347,19 @@ void loop() {
   switch (state) {
     case STATE_READY_TO_RECORD:
       // Switch flipped off
-      if (debounced(RECORD_MODE_SWITCH_INDEX) && !digitalRead(RECORD_MODE_SWITCH_PIN)) {
+      if (recordSwitchDebounced() && !digitalRead(RECORD_MODE_SWITCH_PIN)) {
         state = STATE_READY_TO_PLAYBACK;
-        debounce(RECORD_MODE_SWITCH_PIN);
+        debounceRecordSwitch();
         resetStatus();
       }
 
       // Button pressed
       pressedButtonIndex = getPressedButtonIndex();
       if (pressedButtonIndex != -1) {
+#ifdef DEBUG
         Serial.print("Pressed button ");
         Serial.println(pressedButtonIndex);
+#endif
         irrecv.resume();
         recordingButtonId = pressedButtonIndex;
         state = STATE_RECORDING;
@@ -209,10 +367,9 @@ void loop() {
       }
 
       break;
-    case STATE_RECORDING: 
+    case STATE_RECORDING:
       // IR command received
-      if (irrecv.decode(&results) && commandPtr < 16) {
-        Serial.println("I'm here!");
+      if (irrecv.decode(&results) && millis() > receiver_paused_until && commandPtr < 16) {
         // -1 is UNKNOWN. We can't accept any of those because we need the protocol
         // in order to transmit the command later. Flash the status LED red to show
         // that something's gone wrong.
@@ -227,28 +384,27 @@ void loop() {
         } else {
           SavedCommand command;
 
-          command.size = getCorrectedRawLength(&results);
-          command.value = results.command;
+          command.size = results.bits;
+          command.value = results.value;
           command.protocol = results.decode_type;
           commands[commandPtr] = command;
+          
+#ifdef DEBUG
+          Serial.println(resultToHumanReadableBasic(&results));
+          Serial.println("Encoded command is:");
+          print(command);
+#endif
 
           commandPtr++;
 
+          // Flash blue!
           setStatus(false, false, true);
           delay(100);
           setStatus(true, false, false);
-
-          Serial.println("Got command: ");
-          Serial.println(resultToHumanReadableBasic(&results));
-          Serial.print("Size: ");
-          Serial.println(command.size);
-          Serial.print("Value: ");
-          Serial.println(command.value);
-          Serial.print("Protocol: ");
-          Serial.println(command.protocol);
-          Serial.print("Total Length: ");
-          Serial.println(commandPtr);
         }
+
+        // We won't accept any more commands until this time
+        receiver_paused_until = millis() + IR_RECEIVER_INTER_COMMAND_PAUSE;
       }
       yield;
 
@@ -257,36 +413,16 @@ void loop() {
       if (pressedButtonIndex == recordingButtonId) {
         state = STATE_READY_TO_RECORD;
 
-        memcpy(saveData.savedMacros[recordingButtonId], commands, commandPtr);
-        saveData.macroLength[recordingButtonId] = commandPtr;
-
-        
-        Serial.println("To be saved:");
         for (int i = 0; i < commandPtr; i++) {
           SavedCommand command = commands[i];
-          Serial.print("Command number: ");
-          Serial.println(i);
-          Serial.print("Size: ");
-          Serial.println(command.size);
-          Serial.print("Value: ");
-          Serial.println(command.value);
-          Serial.print("Protocol: ");
-          Serial.println(command.protocol);
+          saveData.savedMacros[recordingButtonId][i].size = command.size;
+          saveData.savedMacros[recordingButtonId][i].value = command.value;
+          saveData.savedMacros[recordingButtonId][i].protocol = command.protocol;
         }
+        saveData.macroLength[recordingButtonId] = commandPtr;
 
-        Serial.println("Saved");
-        for (int i = 0; i < saveData.macroLength[recordingButtonId]; i++) {
-          SavedCommand command = saveData.savedMacros[recordingButtonId][i];
-          Serial.print("Command number: ");
-          Serial.println(i);
-          Serial.print("Size: ");
-          Serial.println(command.size);
-          Serial.print("Value: ");
-          Serial.println(command.value);
-          Serial.print("Protocol: ");
-          Serial.println(command.protocol);
-        }
-        
+        stashNewMacro(recordingButtonId);
+
         recordingButtonId = -1;
 
         setStatus(true, true, false);
@@ -294,31 +430,32 @@ void loop() {
 
       yield;
 
-      if (millis() > nextButtonScan) {
+      if (millis() > nextSwitchScan) {
         // Switch flipped off
-        if (debounced(RECORD_MODE_SWITCH_INDEX) && !digitalRead(RECORD_MODE_SWITCH_PIN)) {
+        if (recordSwitchDebounced() && !digitalRead(RECORD_MODE_SWITCH_PIN)) {
           state = STATE_READY_TO_PLAYBACK;
-          debounce(RECORD_MODE_SWITCH_PIN);
+          debounceRecordSwitch();
           resetStatus();
         }
 
-        nextButtonScan = millis() + 1000;
+        nextSwitchScan = millis() + SWITCH_SCAN_INTERVAL;
       }
-        
+
       // These are all required if we move to any other state
       if (state != STATE_RECORDING) {
         irrecv.pause();
-        commandPtr = 0;
+        commandPtr = 0; // This effectively clears out the stored macro
       }
-      
+
       break;
     case STATE_READY_TO_PLAYBACK:
       // Switch flipped on
-      if (debounced(RECORD_MODE_SWITCH_INDEX) && digitalRead(RECORD_MODE_SWITCH_PIN)) {
+      if (recordSwitchDebounced() && digitalRead(RECORD_MODE_SWITCH_PIN)) {
         state = STATE_READY_TO_RECORD;
-        debounce(RECORD_MODE_SWITCH_PIN);
+        debounceRecordSwitch();
 
         setStatus(true, true, false);
+        return;
       }
 
       // Button pressed
@@ -326,31 +463,16 @@ void loop() {
       if (pressedButtonIndex != -1) {
         setStatus(false, false, true);
 
-        Serial.print("Playing macro for button ");
-        Serial.println(pressedButtonIndex);
-
         for (int i = 0; i < saveData.macroLength[pressedButtonIndex]; i++) {
-          Serial.println(saveData.savedMacros[pressedButtonIndex][i].value);
-          if (!irsend.send(
-            saveData.savedMacros[pressedButtonIndex][i].protocol, 
-            saveData.savedMacros[pressedButtonIndex][i].value, 
-            saveData.savedMacros[pressedButtonIndex][i].size
-            )
-          ) {
-            setStatus(true, false, false);
-            delay(200);
-            resetStatus();
-            delay(200);
-            setStatus(true, false, false);
-            delay(200);
-            resetStatus();
-            delay(200);
-            setStatus(true, false, false);
-            delay(200);
-            resetStatus();
-            delay(200);
-          } else {
-            delay(500);
+          irsend.send(
+                saveData.savedMacros[pressedButtonIndex][i].protocol,
+                saveData.savedMacros[pressedButtonIndex][i].value,
+                saveData.savedMacros[pressedButtonIndex][i].size,
+                IR_SEND_REPEATS);
+          
+          if (i < saveData.macroLength[pressedButtonIndex] - 1) {
+            // Delay between commands in case the receiving device needs some time
+            delay(COMMAND_PLAYBACK_INTERVAL);
           }
         }
 
